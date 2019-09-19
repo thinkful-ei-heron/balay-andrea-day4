e46ff8f1025fa7b03d0c3a3f16ba2a628660e8ab
balayaydemir <balayaydemir@gmail.com>
2019-09-19 10:03:16 -0700

    cat_carousel_final

--- jquery.js
+++ jquery.js
@@ -1,4 +1,15 @@
 
-$('.thumbnail').on('click', e => {
-    console.log(e.target);
+'use strict';
+
+function handleCatClicks() {
+  $('.thumbnail').on('click', event => {
+    //console.log(event.target);
+    const targetCat = $(event.currentTarget).find('img').attr('src');
+    const otherCats = $(event.currentTarget).find('img').attr('alt');
+    
+    $('.hero img').attr('src', targetCat).attr('alt', otherCats);
+    
   });
+}
+    
+$(handleCatClicks);
